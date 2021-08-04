3aa0a1171ba97856b5557065fdecfb70d6431855
LynaElHaffaf <87149881+LynaElHaffaf@users.noreply.github.com>
2021-08-04 12:48:36 -0400

    Update ROIpractice.py

--- ROIpractice.py
+++ ROIpractice.py
@@ -4,3 +4,10 @@ import os
 roi_data = "rois.json"
 if os.path.exists(roi_data):
     print ('this file exists')
+
+#To read the ROI json file  
+import pandas as pd
+import json
+file_path = "/Users/lynaelhaffaf/Documents/rois.json"
+df = pd.read_json(file_path)
+df
